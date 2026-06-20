c6bba9cb634b1d0e3c6b29d50393eeee3923c09a
StefanieAvila05 <stefanie.avila2005@outlook.es>
2026-06-20 17:28:07 -0500

    refactor: ajustar codigos de respuesta HTTP de acuerdo al estandar REST

--- app/controller/endpoints.py
+++ app/controller/endpoints.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status
 from app.service.business_logic import ClinicaService
-
-# Definicion de enrutadores publicos de la API
 
 router = APIRouter(prefix="/api/citas", tags=["Módulo Citas Externas"])
 
 @router.get("/{cedula}")
 def get_citas_por_cedula(cedula: str):
+    # Invocación de la lógica de negocio central
     resultado = ClinicaService.consultar_agenda_externa(cedula)
+    
+    # Manejo explícito de excepciones utilizando códigos estandarizados REST
     if "error" in resultado:
-        raise HTTPException(status_code=404, detail=resultado["error"])
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=resultado["error"]
+        )
+        
     return resultado
