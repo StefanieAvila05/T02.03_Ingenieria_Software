a73a151c4a5af9345135b8012ef5c77390ae398d
StefanieAvila05 <stefanie.avila2005@outlook.es>
2026-06-28 15:15:55 -0500

    test: implementar verificacion de disponibilidad de la ruta raiz

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -9,3 +9,14 @@ def test_endpoint_raiz():
     respuesta = cliente.get("/")
     assert respuesta.status_code == 200
     assert respuesta.json() == {"status": "Servicio Backend Operativo con éxito"}
+
+def test_endpoint_citas_exitoso():
+    """Prueba la respuesta HTTP 200 al consultar un paciente existente"""
+    respuesta = cliente.get("/api/citas/0999999999")
+    assert respuesta.status_code == 200
+    assert "paciente" in respuesta.json()
+
+def test_endpoint_citas_no_encontrado():
+    """Prueba la respuesta HTTP 404 cuando la cedula no existe"""
+    respuesta = cliente.get("/api/citas/9999999999")
+    assert respuesta.status_code == 404
